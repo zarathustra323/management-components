3a0b4bb3484c960fb41746a5b5910eaf064c39e6
Jacob Bare <jacob@limit0.io>
2019-10-08 17:52:14 -0500

    Create scheduling buttons

---
+++ src/components/scheduling/buttons/delete.vue
@@ -0,0 +1,13 @@
+<template>
+  <button type="button" class="btn btn-scheduling btn-scheduling--delete">
+    <icon-x />
+  </button>
+</template>
+
+<script>
+import IconX from '../../icons/x.vue';
+
+export default {
+  components: { IconX },
+};
+</script>
---
+++ src/components/scheduling/buttons/edit.vue
@@ -0,0 +1,13 @@
+<template>
+  <button type="button" class="btn btn-scheduling btn-scheduling--edit">
+    <icon-pencil />
+  </button>
+</template>
+
+<script>
+import IconPencil from '../../icons/pencil.vue';
+
+export default {
+  components: { IconPencil },
+};
+</script>
