c8760b1c25692b2346a05bb249293847f3647bf5
Jacob Bare <jacob@limit0.io>
2019-10-10 13:42:12 -0500

    Add api hostname and change tenant

--- src/apollo/provider.js
+++ src/apollo/provider.js
@@ -2,8 +2,8 @@ import VueApollo from 'vue-apollo';
 import createApolloClient from './create-client';
 
 // @todo determine how to configure this!
-const uri = 'http://0.0.0.0:11001';
-const headers = { 'x-tenant-key': 'indm_all' };
+const uri = 'http://0.0.0.0:11000';
+const headers = { 'x-tenant-key': 'pmmi_all', 'x-base4-api-hostname': 'pmmi.manage.baseplatform.io' };
 
 export default new VueApollo({
   defaultClient: createApolloClient({ uri, headers }),
