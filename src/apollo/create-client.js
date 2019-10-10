0c63ce3b6ee0a663b7cedf36f4cf42e0048e88b0
Jacob Bare <jacob@limit0.io>
2019-10-10 14:21:25 -0500

    Name the apollo client

--- src/apollo/create-client.js
+++ src/apollo/create-client.js
@@ -4,6 +4,7 @@ import { InMemoryCache } from 'apollo-cache-inmemory';
 import fragmentMatcher from './fragment-matcher';
 
 export default ({ uri, headers }) => new ApolloClient({
+  name: 'Management Components',
   link: createHttpLink({ uri, headers }),
   cache: new InMemoryCache({ fragmentMatcher }),
 });
