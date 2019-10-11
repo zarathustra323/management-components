754225bcc1e077d11dadbe5c8af6c81de6f500b0
Jacob Bare <jacob@limit0.io>
2019-10-10 19:28:08 -0500

    Create check and checklist icons

---
+++ src/components/icons/check.vue
@@ -0,0 +1,29 @@
+
+<template>
+  <icon-wrapper
+    name="check"
+    :tag="tag"
+    :class-name="className"
+  >
+    <!-- eslint-disable-next-line -->
+    <svg xmlns="http://www.w3.org/2000/svg" width="12" height="16" viewBox="0 0 12 16"><path fill-rule="evenodd" d="M12 5l-8 8-4-4 1.5-1.5L4 10l6.5-6.5L12 5z"/></svg>
+  </icon-wrapper>
+</template>
+
+<script>
+import IconWrapper from './_wrapper.vue';
+
+export default {
+  components: { IconWrapper },
+  props: {
+    className: {
+      type: String,
+      default: null,
+    },
+    tag: {
+      type: String,
+      default: 'span',
+    },
+  },
+};
+</script>
