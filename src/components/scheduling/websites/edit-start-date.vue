cb828de75731517cce6c57937c17e2d9446835cc
Jacob Bare <jacob@limit0.io>
2019-10-11 18:51:54 -0500

    Create edit start date component and style

---
+++ src/components/scheduling/websites/edit-start-date.vue
@@ -0,0 +1,38 @@
+<template>
+  <datetime
+    v-model="start"
+    type="datetime"
+    title="Start Date"
+    input-class="form-control"
+    :hour-step="1"
+    :minute-step="5"
+  />
+</template>
+
+<script>
+import { Datetime } from 'vue-datetime';
+
+export default {
+  props: {
+    date: {
+      type: Date,
+      required: true,
+    },
+  },
+
+  components: {
+    Datetime,
+  },
+
+  computed: {
+    start: {
+      get() {
+        return this.date.toISOString();
+      },
+      set() {
+        return null;
+      },
+    },
+  },
+};
+</script>
