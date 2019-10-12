ce56868edab3253c8d3347534ff920d0cdfcac79
Jacob Bare <jacob@limit0.io>
2019-10-11 20:11:00 -0500

    Style date picker buttons and add icons

--- src/components/scheduling/websites/edit-start-date.vue
+++ src/components/scheduling/websites/edit-start-date.vue
@@ -10,11 +10,28 @@
     :phrases="{ ok: 'OK', cancel: 'Cancel' }"
     use12-hour
     auto
-  />
+  >
+    <template slot="button-cancel" class="foo">
+    <!-- <fa :icon="['far', 'times']"></fa> -->
+      <x-icon /> Cancel
+    </template>
+    <template slot="button-confirm" slot-scope="scope">
+      <!-- <fa :icon="['fas', 'check-circle']"></fa> -->
+      <span v-if='scope.step === "time"'>
+        <check-icon /> OK
+      </span>
+      <span v-else>
+        Next <chevron-right-icon />
+      </span>
+    </template>
+  </datetime>
 </template>
 
 <script>
 import { Datetime } from 'vue-datetime';
+import CheckIcon from '../../icons/check.vue';
+import XIcon from '../../icons/x.vue';
+import ChevronRightIcon from '../../icons/chevron-right.vue';
 
 export default {
   props: {
@@ -25,7 +42,10 @@ export default {
   },
 
   components: {
+    CheckIcon,
+    ChevronRightIcon,
     Datetime,
+    XIcon,
   },
 
   computed: {
