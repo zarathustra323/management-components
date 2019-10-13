209953c8d09d241ed30266442d7ddaec964c7064
Jacob Bare <jacob@limit0.io>
2019-10-13 13:22:15 -0500

    Create common schedule display date component

---
+++ src/components/scheduling/display-date.vue
@@ -0,0 +1,41 @@
+<template>
+  <time class="schedule-date" v-if="momentDate" :datetime="momentDate.toISOString()">
+    <span v-if="label" class="schedule-date__label">
+      {{ label }}
+    </span>
+    {{ formatted }}
+  </time>
+</template>
+
+<script>
+import moment from 'moment';
+
+export default {
+  props: {
+    value: {
+      type: Date,
+      default: null,
+    },
+    label: {
+      type: String,
+      default: null,
+    },
+    format: {
+      type: String,
+      default: 'MMM Do, YYYY, h:mm A',
+    },
+  },
+
+  computed: {
+    momentDate() {
+      if (this.value) return moment(this.value);
+      return null;
+    },
+    formatted() {
+      const { momentDate } = this;
+      if (momentDate) return momentDate.format(this.format);
+      return '';
+    },
+  },
+};
+</script>
