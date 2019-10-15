d70cd2cdfcf3d03dcec01218f7a0235f6ba9f749
Jacob Bare <jacob@limit0.io>
2019-10-15 11:38:22 -0500

    Remove redudant divs

--- src/components/scheduling/websites/create.vue
+++ src/components/scheduling/websites/create.vue
@@ -2,17 +2,15 @@
   <form class="bmc-schedule-tab__create" @submit.prevent="save">
     <div class="bmc-schedule-tab__header bmc-schedule-tab__header--with-buttons">
       <span>Add Schedules</span>
-      <div>
-        <add-button
-          ref="button"
-          button-type="submit"
-          label="Add schedules"
-          loading-label="Adding schedules..."
-          tabindex="-1"
-          :disabled="saveDisabled"
-          :isLoading="isSaving"
-        />
-      </div>
+      <add-button
+        ref="button"
+        button-type="submit"
+        label="Add schedules"
+        loading-label="Adding schedules..."
+        tabindex="-1"
+        :disabled="saveDisabled"
+        :isLoading="isSaving"
+      />
     </div>
     <div class="bmc-schedule-tab__body">
       <select-sections
