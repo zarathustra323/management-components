f1ce87c3b93cc1126e3e4ade473064aa829e28ef
Jacob Bare <jacob@limit0.io>
2019-10-15 07:19:48 -0500

    Create initial newsletter list and list item

---
+++ src/components/scheduling/newsletters/list-item.vue
@@ -0,0 +1,139 @@
+<template>
+  <!-- <edit-schedule
+    v-if="isEditing"
+    :schedule-id="id"
+    :issue="issue"
+    :section="section"
+    @cancel="exitEditMode"
+    @update="exitEditMode"
+  /> -->
+  <div v-if="isEditing" class="bmc-schedule-list__item bmc-schedule-list__item--edit">
+    EDIT
+  </div>
+  <div v-else class="bmc-schedule-list__item bmc-schedule-list__item--view">
+    <div>
+      <div class="bmc-schedule-list__product-name">
+        {{ newsletter.name }}
+      </div>
+      <div class="bmc-schedule-list__schedule-name">
+        {{ section.name }}
+      </div>
+      <display-date :value="start" />
+      <operation-error
+        :error="error"
+        wrapper-class="bmc-operation-error--margin-top"
+        @retry="deleteSchedule"
+        @cancel="cancelDelete"
+      />
+    </div>
+
+    <div class="ml-2">
+      <div class="bmc-button-group" role="group">
+        <edit-button
+          label="Edit schedule"
+          :disabled="isDeleting"
+          @click="editSchedule"
+        />
+        <delete-button
+          label="Delete schedule"
+          loading-label="Deleting schedule..."
+          :disabled="isDeleting"
+          :isLoading="isDeleting"
+          @click="deleteSchedule"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import DisplayDate from '../../display-date.vue';
+// import EditSchedule from './edit.vue';
+import EditButton from '../buttons/edit.vue';
+import DeleteButton from '../buttons/delete.vue';
+import OperationError from '../../operation-error.vue';
+
+export default {
+  /**
+   *
+   */
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    newsletter: {
+      type: Object,
+      required: true,
+    },
+    section: {
+      type: Object,
+      required: true,
+    },
+    deploymentDate: {
+      type: Number,
+      required: true,
+    },
+  },
+
+  data: () => ({
+    isDeleting: false,
+    isEditing: false,
+    error: null,
+  }),
+
+  components: {
+    DisplayDate,
+    EditButton,
+    DeleteButton,
+    OperationError,
+  },
+
+  computed: {
+    start() {
+      if (this.deploymentDate) return new Date(this.deploymentDate);
+      return null;
+    },
+  },
+
+  /**
+   *
+   */
+  methods: {
+    /**
+     *
+     */
+    async editSchedule() {
+      this.isEditing = true;
+    },
+
+    /**
+     *
+     */
+    async deleteSchedule() {
+      this.error = null;
+      this.isDeleting = true;
+      console.log('DELETE');
+      // const input = { id: this.id };
+      // try {
+      //   await this.$apollo.mutate({ mutation, variables: { input }, refetchQueries: ['ListMagazineSchedules'] });
+      // } catch (e) {
+      //   this.error = e;
+      // } finally {
+      //   this.isDeleting = false;
+      // }
+    },
+
+    exitEditMode() {
+      this.isEditing = false;
+    },
+
+    /**
+     *
+     */
+    cancelDelete() {
+      this.error = null;
+    },
+  },
+};
+</script>
--- src/components/scheduling/newsletters/list.vue
+++ src/components/scheduling/newsletters/list.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bmc-schedule-tab__list">
     <div class="bmc-schedule-tab__header">
-      Current Schedules
+      Newsletter Schedules
       <span>({{ totalCount }})</span>
     </div>
     <div v-if="isLoading" class="bmc-schedule-tab__body">
@@ -14,11 +14,31 @@
       >
         None found
       </div>
+      <operation-error
+        :error="error"
+        :can-cancel="false"
+        wrapper-class="bmc-schedule-list__item"
+        @retry="refresh"
+      />
+      <list-item
+        v-for="(schedule) in schedules"
+        :key="schedule.id"
+        :id="schedule.id"
+        :newsletter="schedule.newsletter"
+        :section="schedule.section"
+        :deployment-date="schedule.deploymentDate"
+      />
     </div>
   </div>
 </template>
 
 <script>
+import query from '../../../graphql/scheduling/queries/list-email-schedules';
+import ListItem from './list-item.vue';
+import OperationError from '../../operation-error.vue';
+import LoadingSpinner from '../../loading-spinner.vue';
+import mapNodes from '../../../utils/map-nodes';
+
 export default {
   /**
    *
@@ -39,12 +59,47 @@ export default {
     error: null,
   }),
 
+  components: { OperationError, LoadingSpinner, ListItem },
+
   /**
    *
    */
   computed: {
     isLoading() {
       return this.$apollo.loading;
+    },
+  },
+
+  /**
+   *
+   */
+  methods: {
+    refresh() {
+      this.error = null;
+      this.$apollo.queries.schedules.refresh();
+    },
+  },
+
+  /**
+   *
+   */
+  apollo: {
+    schedules: {
+      query,
+      variables() {
+        const input = {
+          contentId: this.contentId,
+          pagination: { limit: 0 },
+        };
+        return { input };
+      },
+      update({ contentEmailSchedules }) {
+        if (contentEmailSchedules) this.totalCount = contentEmailSchedules.totalCount;
+        return mapNodes(contentEmailSchedules);
+      },
+      error(e) {
+        this.error = e;
+      },
     },
   },
 };
