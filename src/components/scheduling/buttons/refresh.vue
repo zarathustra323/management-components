99fa3b879310e13f17cebcc757b01f10942aaab8
Jacob Bare <jacob@limit0.io>
2019-10-15 08:15:39 -0500

    Create refresh button

---
+++ src/components/scheduling/buttons/refresh.vue
@@ -0,0 +1,40 @@
+<template>
+  <action-button
+    icon="sync"
+    type="primary"
+    :label="label"
+    :outline="false"
+    :disabled="disabled"
+    :is-loading="isLoading"
+    @click="$emit('click')"
+  />
+</template>
+
+<script>
+import ActionButton from '../../action-button.vue';
+
+export default {
+  /**
+   *
+   */
+  props: {
+    label: {
+      type: String,
+      default: 'Edit',
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    isLoading: {
+      type: Boolean,
+      default: false,
+    },
+  },
+
+  /**
+   *
+   */
+  components: { ActionButton },
+};
+</script>
