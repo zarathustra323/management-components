f7e3b40d540fffb1f3b3d4376821cfc78fa438f5
Jacob Bare <jacob@limit0.io>
2019-10-17 11:10:32 -0500

    Load library as a command queue and set to main

--- src/lib.js
+++ src/lib.js
@@ -27,19 +27,35 @@ const loadComponent = async ({
   if (!hasConfigured()) throw new Error('BaseCMS Management Components have not been configured. Run `bmc.configure()` before loading components.');
   if (!components[name]) throw new Error(`No BaseCMS Management Component found for '${name}'`);
   const { default: Component } = await components[name]();
-  const vm = new Vue({
+  new Vue({
     el,
     apolloProvider: getApolloProvider(),
     render: h => h(Component, { props, on }),
   });
   if (debug()) console.info(`Component ${name} mounted.`, { el, props, on });
-  return vm;
 };
 
-const info = () => ({ version, config: getConfig(), components: Object.keys(components) });
+const info = () => {
+  console.info({ version, config: getConfig(), components: Object.keys(components) });
+};
 
-export default {
+const fns = {
   configure,
   loadComponent,
   info,
 };
+
+const bmc = (fn, ...args) => {
+  if (typeof fns[fn] === 'function') return fns[fn](...args);
+  throw new Error(`No BaseCMS command exists for '${fn}'`);
+};
+
+const { isArray } = Array;
+const { bmcQueue } = window;
+
+if (isArray(bmcQueue)) {
+  bmcQueue.forEach((args) => {
+    bmc(...args);
+  });
+}
+window.bmc = bmc;
