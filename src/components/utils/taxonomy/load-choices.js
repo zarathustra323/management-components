c91351837ce710a4f8a8a9b0a6268f807f98e890
Jacob Bare <jacob@limit0.io>
2019-10-17 20:56:30 -0500

    Fix taxonomy query input

--- src/components/utils/taxonomy/load-choices.js
+++ src/components/utils/taxonomy/load-choices.js
@@ -3,7 +3,11 @@ import mapNodes from '../map-nodes';
 import createNode from './create-node';
 
 export default async (apollo, type, { expandedIds = [] } = {}) => {
-  const input = { type };
+  const input = {
+    type,
+    sort: { field: 'name', order: 'asc' },
+    pagination: { limit: 0 },
+  };
   const { data } = await apollo.query({ query, variables: { input } });
   const taxonomies = mapNodes(data.rootTaxonomiesOfType);
   return taxonomies.map(t => createNode(t, { expandedIds }));
--- src/graphql/common/queries/taxonomy-choices.js
+++ src/graphql/common/queries/taxonomy-choices.js
@@ -4,7 +4,7 @@ import taxonomyFragment from '../fragments/taxonomy';
 export default gql`
 
 query TaxonomyChoices(
-  $input: RootTaxonomiesOfTypeQueryInput = { sort: { field: name, order: asc }, pagination: { limit: 0 } },
+  $input: RootTaxonomiesOfTypeQueryInput!,
   $childrenInput: TaxonomyChildrenInput = { sort: { field: name, order: asc }, pagination: { limit: 0 } },
 ) {
   rootTaxonomiesOfType(input: $input) {
