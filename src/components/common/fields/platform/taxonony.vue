40356c0156016db3c0711f848e09ae4b45f204dd
Jacob Bare <jacob@limit0.io>
2019-10-17 20:03:46 -0500

    Create common taxonomy field

---
+++ src/components/common/fields/platform/taxonony.vue
@@ -0,0 +1,99 @@
+<template>
+  <tree-select
+    v-model="currentTaxonomy"
+    value-format="object"
+    :auto-load-root-options="false"
+    :backspace-removes="false"
+    :clearable="clearable"
+    :disabled="disabled"
+    :flat="true"
+    :load-options="loadChoices"
+    :multiple="false"
+    :options="choices"
+    :placeholder="placeholder"
+    :required="required"
+    :show-count="showCount"
+    @open="$emit('open')"
+    @close="$emit('close')"
+    @input="emitChange"
+    search-nested
+  />
+</template>
+
+<script>
+import TreeSelect, { LOAD_ROOT_OPTIONS } from '@riophae/vue-treeselect';
+import loadChoices from '../../../utils/taxonomy/load-choices';
+import createTaxonomyNode from '../../../utils/taxonomy/create-node';
+
+export default {
+  props: {
+    type: {
+      type: String,
+      required: true,
+    },
+    taxonomy: {
+      type: Object,
+      default: null,
+    },
+    placeholder: {
+      type: String,
+      default: 'Select taxonomy; type to filter...',
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    clearable: {
+      type: Boolean,
+      default: false,
+    },
+    showCount: {
+      type: Boolean,
+      default: true,
+    },
+    required: {
+      type: Boolean,
+      default: true,
+    },
+    nodeOptions: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+
+  data: () => ({
+    choices: null,
+  }),
+
+  components: {
+    TreeSelect,
+  },
+
+  computed: {
+    currentTaxonomy: {
+      get() {
+        return createTaxonomyNode(this.taxonomy, this.nodeOptions);
+      },
+      set() {
+      },
+    },
+  },
+
+  methods: {
+    emitChange(choice) {
+      const taxonomy = choice ? choice.model : null;
+      this.$emit('change', taxonomy);
+    },
+
+    async loadChoices({ action }) {
+      if (action === LOAD_ROOT_OPTIONS) {
+        const expandedIds = [];
+        this.choices = await loadChoices(this.$apollo, this.type, {
+          ...this.nodeOptions,
+          expandedIds,
+        });
+      }
+    },
+  },
+};
+</script>
