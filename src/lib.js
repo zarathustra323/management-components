8299bc9f0aa42d0334a3ab9cd109cdb746111517
Jacob Bare <jacob@limit0.io>
2019-10-17 20:58:03 -0500

    Add primary category export and example

--- src/lib.js
+++ src/lib.js
@@ -17,6 +17,7 @@ Vue.use(VueApollo);
 const components = {
   scheduling: () => import(/* webpackChunkName: "scheduling" */ './components/scheduling/index.vue'),
   'primary-section': () => import(/* webpackChunkName: "primary-section" */ './components/primary-section/index.vue'),
+  'primary-category': () => import(/* webpackChunkName: "primary-category" */ './components/primary-category/index.vue'),
 };
 
 const loadComponent = async ({
