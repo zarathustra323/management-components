05edd0de78cc4c68d4f4c0792804f717f280f54e
Jacob Bare <jacob@limit0.io>
2019-10-22 14:03:50 -0500

    Remove unused newsletter scheduling utils

--- src/components/scheduling/newsletters/utils/map-sections.js
+++
@@ -1,5 +0,0 @@
-import mapNodes from '../../../utils/map-nodes';
-import createSectionNode from './create-section-node';
-
-export default ({ sections, withNewsletterName = true } = {}) => mapNodes(sections)
-  .map(section => createSectionNode(section, { withNewsletterName }));
