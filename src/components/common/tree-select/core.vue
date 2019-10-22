b4082ed219663dc639d8dbf357b0527eafe0c7a0
Jacob Bare <jacob@limit0.io>
2019-10-22 16:12:33 -0500

    Fix scss variables import

--- src/components/common/tree-select/core.vue
+++ src/components/common/tree-select/core.vue
@@ -196,7 +196,7 @@ export default {
 </script>
 
 <style lang="scss">
-@import "../../scss/variables";
+@import "../../../scss/variables";
 
 .vue-treeselect {
   font-family: $bmc-base-font-family;
